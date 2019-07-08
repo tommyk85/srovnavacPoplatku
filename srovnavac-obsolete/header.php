e6916210c890d9dee3e7778cb6ac25310e8aaffc
Tomas.Keresztes <tomas.keresztes@embedit.cz>
2019-07-08 21:04:01 +0200

    chore/clean-trash

--- srovnavac-obsolete/header.php
+++
@@ -1,8 +0,0 @@
-<TR><TD width=400 height=100 background="..\logo.gif"></TD>
-<TD colspan=2 style="text-align:right; vertical-align:text-top">
-Dnes je <?php date_default_timezone_set('Europe/Prague'); 
-echo Date("d/m/Y", Time());  ?>
-<BR><SPAN style="color:gray; font-style:italic">Poslední aktualizace dne 01/05/2013</SPAN>
-<P style="font-size:32; color:orangered; font-weight:bold; line-height:0">Srovnávač poplatků bank</P>
-</TD></TR>
-
--- srovnavac-obsolete/poplatky-banky/2600.php
+++
@@ -1,1 +0,0 @@
-<?php //echo "poplatky Citi.<BR>"; ?>
