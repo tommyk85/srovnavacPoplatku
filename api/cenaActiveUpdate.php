e52559486b31219ed9ce6e2a8fe98db22f15888d
Tomas.Keresztes <tomas.keresztes@embedit.cz>
2019-07-18 21:51:01 +0200

    REFACT: Zalozit slozku pro API

--- api/cenaActiveUpdate.php
+++ api/cenaActiveUpdate.php
@@ -14,12 +14,4 @@ if ($a1 and $a2) {
     mysql_query("ROLLBACK");
 }
 
-
-// $sql = "START TRANSACTION;
-//     UPDATE ucty_ceny SET ucty_ceny.cena_active=0;
-//     UPDATE ucty_ceny uc inner join (select uc2.cena_ucet_id, max(uc2.cena_platnost_od) max_od from ucty_ceny uc2 where uc2.cena_platnost_od <= current_date group by uc2.cena_ucet_id) max_id 
-// 		ON max_id.cena_ucet_id=uc.cena_ucet_id and max_id.max_od=uc.cena_platnost_od SET uc.cena_active=1;
-//     COMMIT;";
-//     
-// vystup_sql($sql);
 ?>
