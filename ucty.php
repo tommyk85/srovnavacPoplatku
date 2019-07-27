32fa36ba57996d769f351faed5149cfe0f885cba
Tomas.Keresztes <tomas.keresztes@embedit.cz>
2019-07-27 13:36:46 +0200

    CHORE: uprava seznamu zpracovanych beznych uctu

--- ucty.php
+++ ucty.php
@@ -43,7 +43,7 @@ include "pripojeni_sql.php";
 
 ?>
 <BR>
-Na této stránce získáte přehled o účtech a základních informacích k bankám, které jsou zahrnuté ve výpočtech aplikace Srovnávač poplatků bank, nebo budou zahrnuty v nejbližší době. Pokud v seznamu chybí některý účet nebo banka, kterou by jste rád porovnal s ostatními, nebo se Vám již zpracovaný účet nezobrazuje ve výsledcích Srovnávače, dejte mi o tom prosím vědět na kontaktní email, viz sekce <A href="projekt.htm">O projektu</A>, rád odpovím v co nejkratším termínu.<BR>
+Na této stránce získáte přehled o účtech a základních informacích k bankám, které jsou zahrnuté ve výpočtech aplikace Srovnávač poplatků bank, nebo budou zahrnuty v nejbližší době. Pokud v seznamu chybí některý účet nebo banka, kterou by jste rád porovnal s ostatními, nebo se Vám již zpracovaný účet nezobrazuje ve výsledcích Srovnávače, dejte mi o tom prosím vědět na kontaktní email, viz sekce <A href="projekt.php">O projektu</A>, rád odpovím v co nejkratším termínu.<BR>
 
 <DIV class='tab'>
 <H1>Běžné účty - již připravené:</H1> 
@@ -51,12 +51,17 @@ Na této stránce získáte přehled o účtech a základních informacích k ba
 <TR>
 <TH width=250>Název účtu</TH>
 <TH width=200>Název banky</TH>
+<TH width=200>Aktuální sazebník</TH>
+<TH width=50>Www</TH>
 </TR>
 
 <?php
-$kody = "'6210','6100','6800','4000','3030','2700','2310','2010','0800','0600','0300','5500','0100'";
 
-$sql_ucty = "SELECT ucet_nazev, nazev_banky FROM ucty INNER JOIN banky ON ucty.ucet_kod_banky = banky.kod_banky WHERE kod_banky IN ($kody) 
+$sql_ucty = "SELECT ucet_nazev, nazev_banky, cena_platnost_od, ucet_www
+FROM ucty
+INNER JOIN banky ON ucty.ucet_kod_banky = banky.kod_banky
+LEFT JOIN ucty_ceny ON ucty_ceny.cena_ucet_id=ucty.ucet_id
+WHERE ucet_typ like 'bezny%' AND cena_active=1
 ORDER BY nazev_banky ASC, ucet_nazev ASC";
 $ucty = mysql_query($sql_ucty, $id_spojeni);
 if (!$ucty)
@@ -70,7 +75,9 @@ while($radek_ucty = mysql_fetch_assoc($ucty))
 {
      echo "<TR>     
      <TD nowrap>".$radek_ucty['ucet_nazev']."</TD>
-     <TD nowrap>".$radek_ucty['nazev_banky']."</TD></TR>";
+     <TD nowrap>".$radek_ucty['nazev_banky']."</TD>
+     <TD nowrap>".$radek_ucty['cena_platnost_od']."</TD>
+     <TD nowrap><a href='".$radek_ucty['ucet_www']."' target='_blank'>link</a></TD></TR>";
 }
 ?>
 
@@ -84,7 +91,11 @@ while($radek_ucty = mysql_fetch_assoc($ucty))
 <TH width=200>Název banky</TH>
 </TR>
 <?php
-$sql_ucty2 = "SELECT ucet_nazev, nazev_banky FROM ucty INNER JOIN banky ON ucty.ucet_kod_banky = banky.kod_banky WHERE kod_banky NOT IN ($kody) 
+$sql_ucty2 = "SELECT ucet_nazev, nazev_banky
+FROM ucty
+INNER JOIN banky ON ucty.ucet_kod_banky = banky.kod_banky
+LEFT JOIN ucty_ceny ON ucty_ceny.cena_ucet_id=ucty.ucet_id
+WHERE ucet_typ like 'bezny%' AND cena_id IS NULL
 ORDER BY nazev_banky ASC, ucet_nazev ASC";
 $ucty2 = mysql_query($sql_ucty2, $id_spojeni);
 if (!$ucty2)
@@ -115,7 +126,8 @@ while($radek_ucty2 = mysql_fetch_assoc($ucty2))
 </TR>
 
 <?php
-$sql_banky = "SELECT * FROM banky WHERE kod_banky IN ($kody) ORDER BY nazev_banky ASC";
+$sql_banky = "SELECT DISTINCT banky.* FROM banky
+INNER JOIN ucty ON ucty.ucet_kod_banky = banky.kod_banky WHERE ucet_typ like 'bezny%' ORDER BY nazev_banky ASC";
 $banky = mysql_query($sql_banky, $id_spojeni);
 if (!$banky)
 {
