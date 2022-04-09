bef85990b32918e848c51e7e22cdeb9c116756ba
Tomas Keresztes <tom.keres@gmail.com>
2022-04-09 19:06:49 +0200

    FIX(admin): neukladat poplatky bez data platnosti

--- admin_page/popl_bezne_ucty.php
+++ admin_page/popl_bezne_ucty.php
@@ -44,56 +44,58 @@ $tb_sql = isset($_GET['zrizeniTB']) && is_numeric($_GET['zrizeniTB']) ? "tb_Zriz
 ".($_GET['odchozi2_TB'] == Null ? "tb_Odchozi2=Null" : "tb_Odchozi2 = ".$_GET['odchozi2_TB']).", 
 ".($_GET['zrizeniTP_TB'] == Null ? "tb_ZrizeniTP=Null" : "tb_ZrizeniTP = ".$_GET['zrizeniTP_TB']) : "tb_Zrizeni=Null";
 
-
 if(isset($_GET['vlozeni_popl'])){                                         //   VLOZENI NOVYCH POPLATKU
-$sql_vlozit_hlavni = "INSERT INTO ucty_ceny
-(cena_ucet_id, cena_platnost_od, cena_zrizeni, cena_zruseni, cena_vedeni, cena_vedeni_podm, cena_vypis_e, cena_vypis_p, cena_prichozi1, cena_prichozi2, cena_odchozi_tp1, cena_odchozi_tp2, cena_odchozi_online1, cena_odchozi_priorita, cena_inkaso_svoleni, cena_inkaso_odchozi, cena_kontokorent_zrizeni, cena_kontokorent_vedeni, cena_kontokorent_zruseni, cena_koment_JP, cena_koment_PP, cena_koment_trans, cena_koment_inkaso, cena_koment_kontokorent, cena_koment_karta)
-VALUES (".$_GET['ucet'].", '".$_GET['platnostOd']."', 
-".($_GET['zrizeniUctu'] == "" ? "Null, " : $_GET['zrizeniUctu'].", ") 
-.($_GET['zruseniUctu'] == "" ? "Null, " : $_GET['zruseniUctu'].", ")
-.($_GET['vedeniUctu'] == "" ? "Null, " : $_GET['vedeniUctu'].", ")
-.($_GET['vedeniUctu_podm'] == "" ? "Null, " : $_GET['vedeniUctu_podm'].", ")
-.($_GET['vypisE'] == "" ? "Null, " : $_GET['vypisE'].", ")
-.($_GET['vypisP'] == "" ? "Null, " : $_GET['vypisP'].", ") 
-.($_GET['prichozi1'] == "" ? "Null, " : $_GET['prichozi1'].", ")
-.($_GET['prichozi2'] == "" ? "Null, " : $_GET['prichozi2'].", ")
-.($_GET['odchoziTP1'] == "" ? "Null, " : $_GET['odchoziTP1'].", ")
-.($_GET['odchoziTP2'] == "" ? "Null, " : $_GET['odchoziTP2'].", ")
-.($_GET['odchoziOn1'] == "" ? "Null, " : $_GET['odchoziOn1'].", ")
-.($_GET['odchoziP'] == "" ? "Null, " : $_GET['odchoziP'].", ")
-.($_GET['inkSvoleni'] == "" ? "Null, " : $_GET['inkSvoleni'].", ")
-.($_GET['inkOdch'] == "" ? "Null, " : $_GET['inkOdch'].", ")
-.($_GET['kontZrizeni'] == "" ? "Null, " : $_GET['kontZrizeni'].", ")
-.($_GET['kontVedeni'] == "" ? "Null, " : $_GET['kontVedeni'].", ")
-.($_GET['kontZruseni'] == "" ? "Null, " : $_GET['kontZruseni'].", ")
-."'".(htmlspecialchars($_GET['koment_JP'], ENT_QUOTES))."', '".(htmlspecialchars($_GET['koment_PP'], ENT_QUOTES))."', '".(htmlspecialchars($_GET['koment_trans'], ENT_QUOTES))."', '".(htmlspecialchars($_GET['koment_ink'], ENT_QUOTES))."', '".(htmlspecialchars($_GET['koment_kont'], ENT_QUOTES))."', 'bez karty')";
-$vlozit_hlavni = vystup_sql($sql_vlozit_hlavni);
+    if(empty(trim($_GET['platnostOd']))) {
+        echo '<script>alert("Chybí platnost od")</script>';
+    } else {
+        $sql_vlozit_hlavni = "INSERT INTO ucty_ceny
+        (cena_ucet_id, cena_platnost_od, cena_zrizeni, cena_zruseni, cena_vedeni, cena_vedeni_podm, cena_vypis_e, cena_vypis_p, cena_prichozi1, cena_prichozi2, cena_odchozi_tp1, cena_odchozi_tp2, cena_odchozi_online1, cena_odchozi_priorita, cena_inkaso_svoleni, cena_inkaso_odchozi, cena_kontokorent_zrizeni, cena_kontokorent_vedeni, cena_kontokorent_zruseni, cena_koment_JP, cena_koment_PP, cena_koment_trans, cena_koment_inkaso, cena_koment_kontokorent, cena_koment_karta)
+        VALUES (".$_GET['ucet'].", '".$_GET['platnostOd']."',
+        ".($_GET['zrizeniUctu'] == "" ? "Null, " : $_GET['zrizeniUctu'].", ")
+        .($_GET['zruseniUctu'] == "" ? "Null, " : $_GET['zruseniUctu'].", ")
+        .($_GET['vedeniUctu'] == "" ? "Null, " : $_GET['vedeniUctu'].", ")
+        .($_GET['vedeniUctu_podm'] == "" ? "Null, " : $_GET['vedeniUctu_podm'].", ")
+        .($_GET['vypisE'] == "" ? "Null, " : $_GET['vypisE'].", ")
+        .($_GET['vypisP'] == "" ? "Null, " : $_GET['vypisP'].", ") 
+        .($_GET['prichozi1'] == "" ? "Null, " : $_GET['prichozi1'].", ")
+        .($_GET['prichozi2'] == "" ? "Null, " : $_GET['prichozi2'].", ")
+        .($_GET['odchoziTP1'] == "" ? "Null, " : $_GET['odchoziTP1'].", ")
+        .($_GET['odchoziTP2'] == "" ? "Null, " : $_GET['odchoziTP2'].", ")
+        .($_GET['odchoziOn1'] == "" ? "Null, " : $_GET['odchoziOn1'].", ")
+        .($_GET['odchoziP'] == "" ? "Null, " : $_GET['odchoziP'].", ")
+        .($_GET['inkSvoleni'] == "" ? "Null, " : $_GET['inkSvoleni'].", ")
+        .($_GET['inkOdch'] == "" ? "Null, " : $_GET['inkOdch'].", ")
+        .($_GET['kontZrizeni'] == "" ? "Null, " : $_GET['kontZrizeni'].", ")
+        .($_GET['kontVedeni'] == "" ? "Null, " : $_GET['kontVedeni'].", ")
+        .($_GET['kontZruseni'] == "" ? "Null, " : $_GET['kontZruseni'].", ")
+        ."'".(htmlspecialchars($_GET['koment_JP'], ENT_QUOTES))."', '".(htmlspecialchars($_GET['koment_PP'], ENT_QUOTES))."', '".(htmlspecialchars($_GET['koment_trans'], ENT_QUOTES))."', '".(htmlspecialchars($_GET['koment_ink'], ENT_QUOTES))."', '".(htmlspecialchars($_GET['koment_kont'], ENT_QUOTES))."', 'bez karty')";
+        $vlozit_hlavni = vystup_sql($sql_vlozit_hlavni);
 
-$sql_hlavni_id = "SELECT max(cena_id) FROM ucty_ceny";
-$hlavni_id = vystup_sql($sql_hlavni_id);
-$max_id = mysqli_result($hlavni_id, 0, 0);
+        $sql_hlavni_id = "SELECT max(cena_id) FROM ucty_ceny";
+        $hlavni_id = vystup_sql($sql_hlavni_id);
+        $max_id = mysqli_result($hlavni_id, 0, 0);
 
-$sql_vlozit_banking = "INSERT INTO ceny_banking (ID) VALUES ($max_id)";
-$vlozit_banking = vystup_sql($sql_vlozit_banking);
+        $sql_vlozit_banking = "INSERT INTO ceny_banking (ID) VALUES ($max_id)";
+        $vlozit_banking = vystup_sql($sql_vlozit_banking);
 
-if($ib_sql <> "" || $mb_sql <> "" || $tb_sql <> ""){
-$sql_ulozit_banking = "UPDATE ceny_banking SET $ib_sql $mb_sql $tb_sql WHERE ID = $max_id";
-$ulozit_banking = vystup_sql($sql_ulozit_banking);}
+        if($ib_sql <> "" || $mb_sql <> "" || $tb_sql <> ""){
+        $sql_ulozit_banking = "UPDATE ceny_banking SET $ib_sql $mb_sql $tb_sql WHERE ID = $max_id";
+        $ulozit_banking = vystup_sql($sql_ulozit_banking);}
 
+        if(isset($_GET['vloz_popl_vc_vyj']) && $_GET['vloz_popl_vc_vyj'] == 1) {
+        copyVyjimky($_GET['ucet_vzor'], $max_id, 'cena');
+        }
 
-if(isset($_GET['vloz_popl_vc_vyj']) && $_GET['vloz_popl_vc_vyj'] == 1) {
-copyVyjimky($_GET['ucet_vzor'], $max_id, 'cena');
-}
+        if(isset($_GET['vloz_popl_vc_karty']) && $_GET['vloz_popl_vc_karty'] == 1) {
+        copyKarty($_GET['ucet_vzor'], $max_id);
+        }
 
-if(isset($_GET['vloz_popl_vc_karty']) && $_GET['vloz_popl_vc_karty'] == 1) {
-copyKarty($_GET['ucet_vzor'], $max_id);
-}
+        if(isset($_GET['vloz_popl_vc_bal']) && $_GET['vloz_popl_vc_bal'] == 1) {
+        copyBalicky($_GET['ucet_vzor'], $max_id);
+        }
 
-if(isset($_GET['vloz_popl_vc_bal']) && $_GET['vloz_popl_vc_bal'] == 1) {
-copyBalicky($_GET['ucet_vzor'], $max_id);
-}
-
-echo "<meta http-equiv='refresh' content='0;url=/srovnavacPoplatku/admin_page/admin.php?kodBanky=".$_GET['kodBanky']."&ucet=".$_GET['ucet']."&nazevUctu=".$_GET['nazevUctu']."&id=$max_id&r_cena_d=".$_GET['r_cena_d']."&note=Poplatky vloženy - ID $max_id. Můžeš vložit první kartu.#poplatky'>";
+        echo "<meta http-equiv='refresh' content='0;url=/srovnavacPoplatku/admin_page/admin.php?kodBanky=".$_GET['kodBanky']."&ucet=".$_GET['ucet']."&ucetTyp=".$_GET['ucetTyp']."&nazevUctu=".$_GET['nazevUctu']."&id=$max_id&r_cena_d=".$_GET['r_cena_d']."&note=Poplatky vloženy - ID $max_id. Můžeš vložit první kartu.#poplatky'>";
+    }
 }
 
 if(isset($_GET['ulozeni_popl'])){                      // ULOZENÍ ZMEN POPLATKu
@@ -128,14 +130,11 @@ cena_koment_kontokorent = '".(htmlspecialchars($_GET['koment_kont'], ENT_QUOTES)
 WHERE cena_id = ".$_GET['id'];
 $ulozit_poplatky = vystup_sql($sql_ulozit_poplatky);
 
-
 $sql_ulozit_banking = "UPDATE ceny_banking SET $ib_sql $mb_sql $tb_sql WHERE ID = ".$_GET['id'];
 $ulozit_banking = vystup_sql($sql_ulozit_banking);
 
-
 echo "<meta http-equiv='refresh' content='0;url=/srovnavacPoplatku/admin_page/admin.php?kodBanky=".$_GET['kodBanky']."&ucet=".$_GET['ucet']."&nazevUctu=".$_GET['nazevUctu']."&id=".$_GET['id']."&r_cena_d=".$_GET['r_cena_d']."&note=Změny v poplatcích uloženy.#poplatky'>";
 }
-
 
 if($_GET['id'] > 0)
 {
@@ -180,7 +179,6 @@ $kontZrizeni = mysqli_result($cena_d, $radek, 'cena_kontokorent_zrizeni');
 $kontVedeni = mysqli_result($cena_d, $radek, 'cena_kontokorent_vedeni');
 $kontZruseni = mysqli_result($cena_d, $radek, 'cena_kontokorent_zruseni');
 $koment_kont = mysqli_result($cena_d, $radek, 'cena_koment_kontokorent');
-
 }
 
 elseif(isset($_GET['rychla_volba']))
@@ -250,7 +248,7 @@ echo $pocet_zaznamu_cena_d == 0 ? "První zadání. K tomuto účtu zatím nebyl
   echo "<INPUT type='submit' name='oprava_popl' value='Provést změny v poplatcích'>
    <INPUT type='submit' name='s_popl_karty' value='Spravovat poplatky karet k účtu (".mysqli_result($cena_d, $radek, 'pocet_karet').")' accesskey='k'>";
   echo "<span style='letter-spacing:50'> <INPUT type='submit' name='s_ucet' value='Spravovat účet'></span>
-  <BR><a href='../admin_page/admin.php?kodBanky=".$_GET['kodBanky']."&ucet=".$_GET['ucet']."&nazevUctu=".$_GET['nazevUctu']."&id=0&vyber_id=' accesskey='n'>Zadat nové poplatky</a>";
+  <BR><a href='../admin_page/admin.php?kodBanky=".$_GET['kodBanky']."&ucet=".$_GET['ucet']."&ucetTyp=".$_GET['ucetTyp']."&nazevUctu=".$_GET['nazevUctu']."&id=0&vyber_id=' accesskey='n'>Zadat nové poplatky</a>";
   }
   else {
   echo "<INPUT type='submit' name='vlozeni_popl' value='Vložit poplatky'".(!isset($_GET['ucet_vzor']) ? " disabled" : "")." accesskey='v'>";
@@ -347,7 +345,6 @@ $zrizeniTP_IB = mysqli_result($vzor, 0, 'ib_ZrizeniTP');
 $zrizeniTP_MB = mysqli_result($vzor, 0, 'mb_ZrizeniTP'); 
 $zrizeniTP_TB = mysqli_result($vzor, 0, 'tb_ZrizeniTP');  
 $zrizeniTP_vzor = $zrizeniTP = "<span class='help'>Zřízení TP přes IB: $zrizeniTP_IB, MB: $zrizeniTP_MB, TB: $zrizeniTP_TB</span><BR>";
-
 }
 
 else{
@@ -360,7 +357,6 @@ $zrizeniUctu_vzor = $zruseniUctu_vzor = $vedeniUctu_vzor = $vypis_vzor = $pricho
 Platnost Od* <span class='help'>(RRRR-MM-DD)</span>: <INPUT <?php echo ($_GET['id'] == 0 ? "class='chybi' " : ""); ?>type='text' name='platnostOd' value="<?php echo $platnostOd; ?>" size=8 style="text-align:right"<?php echo $popl_read_only; ?>>
 
 <?php
-
 $banka = $_GET['kodBanky'];
 $ucetID = $_GET['ucet'];
 
@@ -466,8 +462,6 @@ echo "Možno zadat až k uloženým základním poplatkům.";}
 else {
 ?>
 
-
-
 <form name='bal' method='post'>
 <div style='margin-bottom:40px;'>
 Název nového balíčku: <input type='text' name='novy_bal_nazev' size=40<?php echo $popl_read_only; ?> /><br />
@@ -483,7 +477,6 @@ if(isset($_POST['novy_bal'])){
 vystup_sql("INSERT INTO balicky (bal_cena_id, bal_nazev, bal_cena, bal_koment) VALUES (".$_GET['id'].", '".$_POST['novy_bal_nazev']."', ".$_POST['novy_bal_cena'].", '".$_POST['novy_bal_koment']."')");
 echo "<meta http-equiv='refresh' content='0;url=/srovnavacPoplatku/admin_page/admin.php?kodBanky=".$_GET['kodBanky']."&ucet=".$_GET['ucet']."&nazevUctu=".$_GET['nazevUctu']."&id=".$_GET['id']."&r_cena_d=".$_GET['r_cena_d']."&note=Balíček ".$_POST['novy_bal_nazev']." přidán.#poplatky'>";
 }
-
 
 $sql_balicky = "SELECT * FROM balicky WHERE bal_cena_id = ".$_GET['id'];
 $balicky = vystup_sql($sql_balicky);
@@ -508,7 +501,6 @@ Popis balíčku: <TEXTAREA name="bal_koment" cols=80 rows=6<?php echo $popl_read
 <H4>Položky v balíčku</H4>
 
 <?php
-
 echo "<p style='text-indent:0px; background-color:#CCFFFF; padding:2px;'>";
 echo "Pole nové pol.: <input type='text' name='bal_pol_pole$id' /><br/>";
 echo "Popis nové pol.: <input type='text' name='bal_pol_popis$id' /><br/>";
@@ -521,12 +513,10 @@ echo "Podmínka pro výběr: <input type='text' name='bal_pol_vyber_podm$id' val
 <?php
 echo "</p>";
 
-
 if(isset($_POST["nova_polozka$id"])){
 vystup_sql("INSERT INTO bal_polozky (bal_id, bal_pole, bal_popis, bal_pocet_trans, bal_pocet_vyber, bal_podm_vyber) VALUES (".$r_balicky['bal_id'].", '".$_POST["bal_pol_pole$id"]."', '".$_POST["bal_pol_popis$id"]."', ".$_POST["bal_pol_pocet$id"].", ".$_POST["bal_pol_vyber$id"].", '".$_POST["bal_pol_vyber_podm$id"]."')");
 echo "<meta http-equiv='refresh' content='0;url=/srovnavacPoplatku/admin_page/admin.php?kodBanky=".$_GET['kodBanky']."&ucet=".$_GET['ucet']."&nazevUctu=".$_GET['nazevUctu']."&id=".$_GET['id']."&r_cena_d=".$_GET['r_cena_d']."&note=Nová položka do balíčku ".$r_balicky['bal_nazev']." přidána.#poplatky'>";
 }
-
 
 $sql_bal_polozky = "SELECT * FROM bal_polozky WHERE bal_id = ".$r_balicky['bal_id'];
 $bal_polozky = vystup_sql($sql_bal_polozky);
--- sql_queries.php
+++ sql_queries.php
@@ -9,11 +9,11 @@ function copyKarty($_cenaIdVzor, $_cenaIdNove) {
   $sql_vzor_karty_koment = "SELECT cena_koment_karta FROM ucty_ceny WHERE cena_id = $_cenaIdVzor";
   $vzor_karty_koment = vystup_sql($sql_vzor_karty_koment);
         
-  $sql_karty_koment = "UPDATE ucty_ceny SET cena_koment_karta = '".(mysql_result($vzor_karty_koment, 0))."' WHERE cena_id = $_cenaIdNove";
+  $sql_karty_koment = "UPDATE ucty_ceny SET cena_koment_karta = '".(mysqli_result($vzor_karty_koment, 0))."' WHERE cena_id = $_cenaIdNove";
   $karty_koment = vystup_sql($sql_karty_koment);
     
   $sql_vyj_k_pocet = "SELECT * FROM vyjimky WHERE cena_id=$_cenaIdVzor and not karta_id is null";
-  $vyj_k_pocet = mysql_num_rows(vystup_sql($sql_vyj_k_pocet));
+  $vyj_k_pocet = mysqli_num_rows(vystup_sql($sql_vyj_k_pocet));
   
   if($vyj_k_pocet > 0) {
     copyVyjimky($_cenaIdVzor, $_cenaIdNove, 'karta');
@@ -45,11 +45,11 @@ function copyVyjimky($_cenaIdVzor, $_cenaIdNove, $_vyjTyp) {
 
 function copyBalicky($_cenaIdVzor, $_cenaIdNove) {
   $balicky = vystup_sql("SELECT * FROM balicky WHERE bal_cena_id=$_cenaIdVzor");
-  while($r_balicky = mysql_fetch_assoc($balicky)) {
+  while($r_balicky = mysqli_fetch_assoc($balicky)) {
     vystup_sql("INSERT INTO balicky (bal_cena_id, bal_nazev, bal_polozek, bal_cena, bal_koment, bal_volitelny)
       SELECT $_cenaIdNove, bal_nazev, bal_polozek, bal_cena, bal_koment, bal_volitelny FROM balicky WHERE bal_id=".$r_balicky['bal_id']);
 
-    $nove_bal_id = mysql_result(vystup_sql("SELECT max(bal_id) FROM balicky"), 0, 0);
+    $nove_bal_id = mysqli_result(vystup_sql("SELECT max(bal_id) FROM balicky"), 0, 0);
     vystup_sql("INSERT INTO bal_polozky (bal_id, bal_pole, bal_popis, bal_pocet_trans, bal_pocet_vyber, bal_podm_vyber)
       SELECT $nove_bal_id, bal_pole, bal_popis, bal_pocet_trans, bal_pocet_vyber, bal_podm_vyber FROM bal_polozky WHERE bal_id=".$r_balicky['bal_id']);
   }
@@ -57,22 +57,22 @@ function copyBalicky($_cenaIdVzor, $_cenaIdNove) {
 
 function getUcetId($_cenaId) {
   $ucet = vystup_sql("SELECT DISTINCT cena_ucet_id FROM ucty_ceny WHERE cena_id=$_cenaId");
-  return mysql_result($ucet, 0, 0);
+  return mysqli_result($ucet, 0, 0);
 }
 
 function existujiVyjimkyCen($_cenaId) {
   $vyjimky = vystup_sql("SELECT count(*) FROM vyjimky WHERE cena_id=$_cenaId and karta_id is null");
-  return mysql_result($vyjimky, 0, 0) > 0;
+  return mysqli_result($vyjimky, 0, 0) > 0;
 }
 
 function existujiKarty($_cenaId) {
   $karty = vystup_sql("SELECT count(*) FROM ceny_karty WHERE karta_cena_id=$_cenaId");
-  return mysql_result($karty, 0, 0) > 0;
+  return mysqli_result($karty, 0, 0) > 0;
 }
 
 function existujiBalicky($_cenaId) {
   $balicky = vystup_sql("SELECT count(*) FROM balicky WHERE bal_cena_id=$_cenaId");
-  return mysql_result($balicky, 0, 0) > 0;
+  return mysqli_result($balicky, 0, 0) > 0;
 }
 
 ?>
